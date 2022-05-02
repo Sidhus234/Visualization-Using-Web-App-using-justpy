310db4b837cbbfb84c5799fe4b4c8ce27076177a
Gursewak Singh Sidhu <sidhus234@gmail.com>
2022-05-02 15:46:15 +0800

    Update simple_app.py

--- simple_app.py
+++ simple_app.py
@@ -2,11 +2,13 @@ import justpy as jp
 
 
 def app():
+    # Initiate a webpage
     wp = jp.QuasarPage()
     # Formmating : https://quasar.dev/style/shadows (refer this link)
     h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews",
-                 classes="text-h1 text-center q-pa-md")
+                 classes="text-h3 text-center q-pa-md")
     p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis")
+    
     return wp
 
 
